96fa6905aaf0c0b0804d27aed4911489c8d1d095
Tom <thomas.spencer@codurance.com>
2020-07-29 13:51:02 +0100

    Add stringbuilder

--- src/main/java/com/codurance/FizzBuzz.java
+++ src/main/java/com/codurance/FizzBuzz.java
@@ -3,19 +3,23 @@ package com.codurance;
 public class FizzBuzz {
 
   public String convert(int input) {
+    StringBuilder result = new StringBuilder();
     if (input < 1){
       return "0";
     }
 
-    if (isDivisibleBy(input, 15)){
-      return "FizzBuzz";
-    }else if (isDivisibleBy(input, 5)){
-      return "Buzz";
-    }else if (isDivisibleBy(input, 3)) {
-      return "Fizz";
+    if (isDivisibleBy(input, 3)){
+      result.append("Fizz");
     }
 
-    return String.valueOf(input);
+    if (isDivisibleBy(input, 5)){
+      result.append("Buzz");
+    }
+
+    if (result.length() == 0){
+      result.append(input);
+    }
+    return result.toString();
   }
 
   private boolean isDivisibleBy(int input, int i) {
